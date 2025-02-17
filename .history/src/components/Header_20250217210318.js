8fc285cf33be97b9066b2b4ce37e4468cfa958c2
rkCodeX <rimshashoukat271@gmail.com>
2025-02-17 21:33:02 +0500

    react js and tailwind setup installed successfully

---
+++ .history/src/components/Header_20250217210318.js
@@ -0,0 +1,10 @@
+function Header(){
+    return(
+      <>
+        <h1>Welcome to our React App</h1>
+        <h2>This is a simple header component</h2>
+      </>
+    );
+}
+
+export default Header;
