34069bc972639630bad3c46c4f51c8a2479e539b
_rk <rimshashoukat271@gmail.com>
2025-03-03 20:37:29 +0500

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ function About(){
                 <h1 className="aboutH2 text-[3.2rem] max-sm:text-[2.8rem] text-[#eee2ca] font-bold select-none">About Me</h1>
                 <div className="aboutUnder w-[4rem] h-[0.30rem] bg-[#fc6c2f] rounded-[5rem] select-none"></div>
                 <p className="aboutPara1 text-[0.9rem] select-none text-justify leading-snug text-[#eee2ca] my-[2rem]">Aspiring Front-End Developer and university student in my 6th semester, skilled in JavaScript, React.js, Tailwind CSS, HTML, and CSS. I am passionate about building responsive and user-friendly web applications. Proficient in Git for version control and effective in collaborative environments. Eager to contribute my skills and creativity to a dynamic organization while advancing my knowledge in web development.</p>
-                <div className="flex flex-row gap-[1rem] max-md:flex-col">
+                <div className="flex flex-row gap-[1rem] w-[100%] max-md:flex-col">
                  <Link to="#contact">
                     <button className="aboutBtn1 transition-all duration-700 hover:shadow-md hover:shadow-[#fc6c2f] border-2 rounded-full px-[1rem] py-[0.5rem] border-[#eee2ca] text-[#fc6c2f] font-bold select-none ">Contact Me</button>
                  </Link>
